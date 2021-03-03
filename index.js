2a3f012fde42a9a6503bd0e346cad9e679b1f714
pablo <encina.pd@gmail.com>
2021-03-03 19:36:59 -0300

    Comienzo de funcionalidad en js.

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ let boton1 = document.getElementById('boton1')
 
 let menu = document.getElementById('menu')
 
+let btn3 = document.getElementById('btn3')
+
 boton1.addEventListener('click', (event) => {
     if (menu.style.display === 'none') {
-        menu.style.display = 'block'
+        menu.style.display = 'block';
+        btn3.style.display = 'block'
+        boton1.style.opacity = ''
     }else{
-        menu.style.display = 'none'
+        btn3.style.opacity = ''
+        menu.style.display = 'none' 
+        boton1.style.display = 'block'
+        
     }
 })
 
+btn3.addEventListener('click', (event)=>{
+    if(menu.style.display === 'block'){
+        btn3.style.display = 'block'
+    }else{
+        btn3.style.display = 'none'
+    }
+})
 
