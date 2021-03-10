6005e0a2468a1e94d498a5921b1b7db0772de735
pablo <encina.pd@gmail.com>
2021-03-10 19:25:57 -0300

    Empezando con js.

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
+/****Funcion para que aparezca el menu hamb */
 let boton1 = document.getElementById('boton1')
 
 let menu = document.getElementById('menu')
 
-let btn3 = document.getElementById('btn3')
-/****Funcion para que aparezca el menu hamb */ 
 boton1.addEventListener('click', (event) => {
-    if (menu.style.display === 'none') {
-       return menu.style.display = 'block'; 
-    }else{
-       return menu.style.display = 'none' 
-    }
+   if (menu.style.display === 'none') {
+      menu.style.display = 'block';
+   } else {
+      menu.style.display = 'none'
+   }
+})
+/*****Aparicion de boton de cierre de menu hamb****************** */
+let boton2 = document.getElementById('boton2')
+
+boton2.addEventListener('click', (event) => {
+   if (boton2.style.display = 'none') {
+      boton2.style.display = 'block'
+      menu.style.display = 'block';
+   }else{
+      boton2.style.display = 'none'
+   }
+})
+/***CLICK en favoritos**** */
+let fav = document.getElementById('fav')
+
+let principal7 = document.getElementById('principal7')
+
+let principal4 = document.getElementById('principal4')
+
+let principal1 = document.getElementById('principal1')
+
+fav.addEventListener('click', (event) => {
+   principal7.style.display = 'block'
+
+   menu.style.display = 'none'
+
+   principal4.style.display = 'none'
+
+
+   principal1.style.display = 'none'
 })
 
-btn3.addEventListener('click', (event)=> {
-    if(btn3.style.display === 'none'){
-       return btn3.style.display = 'block'
-    }else{
-       return menu.style.display = 'none'
-    }
+/*******Click en Mis GIfos ******* */
+
+let misgif = document.getElementById('misgif')
+
+let principal8 = document.getElementById('principal8')
+
+misgif.addEventListener('click', (event) => {
+   principal8.style.display = 'block'
+
+   menu.style.display = 'none'
+
+   principal4.style.display = 'none'
+
+
+   principal1.style.display = 'none'
 })
+
