3c5f8700b5bd3ea4f3ec32aa5bff437c450c09d2
pablo <encina.pd@gmail.com>
2021-03-11 14:53:06 -0300

    Comienzo de funcionalidades a los botones con Javascript

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**MODO NOCTURNO */
+const mod = document.getElementById('mod');
+ 
+mod.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    mod.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+});
+
+
 /****Funcion para que aparezca el menu hamb */
 let boton1 = document.getElementById('boton1')
 
@@ -10,18 +19,9 @@ boton1.addEventListener('click', (event) => {
       menu.style.display = 'none'
    }
 })
-/*****Aparicion de boton de cierre de menu hamb****************** */
-let boton2 = document.getElementById('boton2')
 
-boton2.addEventListener('click', (event) => {
-   if (boton2.style.display = 'none') {
-      boton2.style.display = 'block'
-      menu.style.display = 'block';
-   }else{
-      boton2.style.display = 'none'
-   }
-})
-/***CLICK en favoritos**** */
+/***CLICK en favoritos**Mobile** */
+
 let fav = document.getElementById('fav')
 
 let principal7 = document.getElementById('principal7')
@@ -31,17 +31,18 @@ let principal4 = document.getElementById('principal4')
 let principal1 = document.getElementById('principal1')
 
 fav.addEventListener('click', (event) => {
-   principal7.style.display = 'block'
 
-   menu.style.display = 'none'
+    menu.style.display = 'none'
 
-   principal4.style.display = 'none'
+    principal4.style.display = 'none'
 
 
-   principal1.style.display = 'none'
+    principal1.style.display = 'none'
+
+   return principal7.style.display = 'block'
 })
 
-/*******Click en Mis GIfos ******* */
+/*******Click en Mis GIfos ****Mobile*** */
 
 let misgif = document.getElementById('misgif')
 
@@ -57,4 +58,3 @@ misgif.addEventListener('click', (event) => {
 
    principal1.style.display = 'none'
 })
-
