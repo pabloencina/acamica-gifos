a08c2a766b1a0a0038c41c77ba33352960658ea1
pablo <encina.pd@gmail.com>
2021-03-18 15:56:26 -0300

    Trabajo con la API de Gifos.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**MODO NOCTURNO */
+/*
 const mod = document.getElementById('mod');
- 
+
 mod.addEventListener('click', () => {
-    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
-    mod.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
+   document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+   mod.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active'
 });
 
 
@@ -32,14 +33,15 @@ let principal1 = document.getElementById('principal1')
 
 fav.addEventListener('click', (event) => {
 
-    menu.style.display = 'none'
+   menu.style.display = 'none'
 
-    principal4.style.display = 'none'
+   principal4.style.display = 'none'
 
+   principal1.style.display = 'none'
 
-    principal1.style.display = 'none'
+   principal7.style.display = 'block'
+   // }
 
-   return principal7.style.display = 'block'
 })
 
 /*******Click en Mis GIfos ****Mobile*** */
@@ -58,3 +60,68 @@ misgif.addEventListener('click', (event) => {
 
    principal1.style.display = 'none'
 })
+
+/**Consumir gifs desde la  API */
+'api.giphy.com/v1/gifs/trending?api_key='
+
+'api.giphy.com/v1/gifs/search'
+
+'https://api.giphy.com/v1/gifs/trending?api_key=boZGHaAmzirlZl5OiViZEx7vayQzDZoY&limit=25&rating=g'
+
+
+let principal2 = document.getElementById('principal2')
+
+const api_key = 'boZGHaAmzirlZl5OiViZEx7vayQzDZoY'
+
+const url_base = 'https://api.giphy.com/v1/gifs/trending?api_key=boZGHaAmzirlZl5OiViZEx7vayQzDZoY&limit=12&search=mascotas'
+
+let mostrartrending = async () => {
+   try {
+      let resultado = await fetch(url_base + api_key)
+
+      let json = await resultado.json()
+
+      json.data.forEach(trending => {
+
+         principal2.innerHTML += `
+         <div class='foto1'>
+            <img class='foto' src='${trending.images.fixed_height.url}>
+         
+            <img src='${trending.images.fixed_height.url}>
+         </div>
+         `
+
+      });
+   } catch (error) {
+   }
+}
+
+mostrartrending()
+
+/**Buscador desde la API */
+
+let buscador1 = document.getElementById('buscador1')
+
+const url_base_buscador = 'https://api.giphy.com/v1/gifs/search?api_key=boZGHaAmzirlZl5OiViZEx7vayQzDZoY&limit=12&q='
+
+buscador1.addEventListener('keypress', async (e) => {
+
+   if (e.key === 'Enter') {
+
+      principal2.innerHTML = ``
+      let resultado = await fetch(url_base_buscador + e.target.value)
+
+      let json = await resultado.json()
+      json.data.forEach(trending => {
+         principal2.innerHTML += `
+         <div class='foto1'>
+            <img class='foto' src='${trending.images.fixed_height.url}>
+         
+            <img src='${trending.images.fixed_height.url}>
+         </div>
+         `
+      });
+   
+   }
+})
+
