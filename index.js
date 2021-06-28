3baea647a3f8ac5131a4455b675db8acc435b795
Pablo <encina.pd@gmail.com>
2021-06-28 07:44:33 -0300

    MOBILE: Al iniciar, el menú hamburguesa se encuentra desplegado.

--- index.js
+++ index.js
@@ -324,17 +324,16 @@ function setTheme(themeName) {
 }
 /****Funcion para que aparezca el menu hamb */
 boton1.addEventListener('click', () => {
-   if (menu.style.display === 'none') {
-      menu.style.display = 'block';
-      lupa.style.visibility = 'hidden'
-      //btn3.style.display = 'block'
-      //boton1.style.visibility = 'hidden'
-
-   } else {
+   if (menu.style.display === 'block') {
       menu.style.display = 'none'
       lupa.style.visibility = 'visible'
       //boton1.style.visibility = 'visible'
       //btn3.style.display = 'none'
+   } else {
+      menu.style.display = 'block';
+      lupa.style.visibility = 'hidden'
+      //btn3.style.display = 'block'
+      //boton1.style.visibility = 'hidden'
    }
 })
 /*
